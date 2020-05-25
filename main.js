4cb48ee5161482001ac44c49a0cb6eac0325ba3c
OisinDavey <oisindavey02@gmail.com>
2020-05-25 14:54:21 +0100

    Added Irish Links

--- main.js
+++ main.js
@@ -2982,11 +2982,12 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;/**
 /*!***************************!*\
   !*** ./src/app/Papers.ts ***!
   \***************************/
-/*! exports provided: HLMathsPapers, HLIrishPapers, HLDCGPapers, HLPhysicsPapers, HLAppliedMathsPapers, OLEnglishPapers, OLIrishPapers, OLMathsPapers */
+/*! exports provided: IrishAurals, HLMathsPapers, HLIrishPapers, HLDCGPapers, HLPhysicsPapers, HLAppliedMathsPapers, OLEnglishPapers, OLIrishPapers, OLMathsPapers */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "IrishAurals", function() { return IrishAurals; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HLMathsPapers", function() { return HLMathsPapers; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HLIrishPapers", function() { return HLIrishPapers; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HLDCGPapers", function() { return HLDCGPapers; });
@@ -2995,6 +2996,64 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "OLEnglishPapers", function() { return OLEnglishPapers; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "OLIrishPapers", function() { return OLIrishPapers; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "OLMathsPapers", function() { return OLMathsPapers; });
+const IrishAurals = [
+    {
+        year: "2019",
+        link: "https://www.examinations.ie/archive/exampapers/2019/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2018",
+        link: "https://www.examinations.ie/archive/exampapers/2018/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2017",
+        link: "https://www.examinations.ie/archive/exampapers/2017/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2016",
+        link: "https://www.examinations.ie/archive/exampapers/2016/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2015",
+        link: "https://www.examinations.ie/archive/exampapers/2015/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2014",
+        link: "https://www.examinations.ie/archive/exampapers/2014/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2013",
+        link: "https://www.examinations.ie/archive/exampapers/2013/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2012",
+        link: "https://www.examinations.ie/archive/exampapers/2012/LC001ZLPO17IV.mp3",
+    },
+    {
+        year: "2011",
+        link: "https://www.examinations.ie/archive/exampapers/2011/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2010",
+        link: "https://www.examinations.ie/archive/exampapers/2010/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2009",
+        link: "https://www.examinations.ie/archive/exampapers/2009/LC001ZLP017IV.mp3",
+    },
+    {
+        year: "2008",
+        link: "https://www.examinations.ie/archive/exampapers/2008/LC001ZLPO17IV.mp3",
+    },
+    {
+        year: "2007",
+        link: "https://www.examinations.ie/archive/exampapers/2007/LC001ZLP00IV.mp3",
+    },
+    {
+        year: "2006",
+        link: "https://drive.google.com/open?id=1yRkjjeqKsXFNdib2gz1PlcZYzWBgifdZ",
+    },
+];
 const HLMathsPapers = [
     {
         tipe: "ExamPaper",
@@ -3345,7 +3404,242 @@ const HLMathsPapers = [
         link: "https://drive.google.com/open?id=1UlNcocsJNVJ0Exy_5HlhcDTJXOdPARWj",
     },
 ];
-const HLIrishPapers = [];
+const HLIrishPapers = [
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2019",
+        link: "https://drive.google.com/open?id=1_DmSN52N1ilR0r_jxgWfWH3SF6OycIs4",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2019",
+        link: "https://drive.google.com/open?id=1kiC8s9d9ktqCvZ6sU3hd-iMRGY6xMxCO",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2018",
+        link: "https://drive.google.com/open?id=1F9NXEE4IqGimXAIjRxw3samjCrI1bpdo",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2018",
+        link: "https://drive.google.com/open?id=1Ywp-M6-Cbxc3XUcTWtoHkD3nERDyX1B6",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2017",
+        link: "https://drive.google.com/open?id=1KjvONO332ZYjfCrpDkL7tcVgybLXWe72",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2017",
+        link: "https://drive.google.com/open?id=1DLcY_KE3WV-X9e34OH5A6yw9iktusvWS",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2016",
+        link: "https://drive.google.com/open?id=198rISurdz4msprP994WPEjGf0LRIIcFS",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2016",
+        link: "https://drive.google.com/open?id=1KSwTQCxfKBT4X0SO5lqJF4R1RH0Q9pa-",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2015",
+        link: "https://drive.google.com/open?id=1Li1e85DS2k3xtR7kQZiBsfwcLoN7k3IU",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2015",
+        link: "https://drive.google.com/open?id=1EH_hBlSJPxtn6HGhWXibTaNFa11_rSWE",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2014",
+        link: "https://drive.google.com/open?id=1DPo4tRJ3qf5ttBEBH7SUVFX22VfLirGC",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2014",
+        link: "https://drive.google.com/open?id=1VsU7aKKT1Nh98E78yrcH-Hhu3D1jK7uE",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2013",
+        link: "https://drive.google.com/open?id=1HqB4hTAs1MAhYfyJvsH_3C09KdszUhYZ",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2013",
+        link: "https://drive.google.com/open?id=1TOxOsQt1Y_VTIr78fVgT_tq1ovH1LQkg",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2012",
+        link: "https://drive.google.com/open?id=1H8Zbb5HPgd4FmQ4NDEKbigondZ5m2oUJ",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2012",
+        link: "https://drive.google.com/open?id=1Oy2opsbPrZUlI7xDp3Nst79aGWhGDOsP",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2011",
+        link: "https://drive.google.com/open?id=1DzgTI3stzAoeB7S-J4t4KTBRlsFTtvmv",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2011",
+        link: "https://drive.google.com/open?id=1gy9IZzLurcdu2gMZYhlqcOqJNXR0f0HG",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2010",
+        link: "https://drive.google.com/open?id=1qrqB3VOrZ9UKaWnl1qpMl4QxsqkqjIT4",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2010",
+        link: "https://drive.google.com/open?id=1QJKWQrJV-AOwwfH025KIKOurr96bJ0XT",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2009",
+        link: "https://drive.google.com/open?id=10vMUMv99HiLvXVH_tedVuPIcPXg9qNKM",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2009",
+        link: "https://drive.google.com/open?id=1FleO4arO4Yn86oAE45US6Jh6k1agU5Un",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2008",
+        link: "https://drive.google.com/open?id=1VjugwBpDIFVVJAqVmh2rpINmuj8CjES2",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2008",
+        link: "https://drive.google.com/open?id=1dRHhCaOa0arkN9ElPE4QFI9k0B2dEx8u",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2007",
+        link: "https://drive.google.com/open?id=1UlsCV9fMb4VG_KJKW50Q69GBc8Lq0t68",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2007",
+        link: "https://drive.google.com/open?id=1YuBG5KroWT3fPhkvj0XPd3MI9DgWnf0d",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2006",
+        link: "https://drive.google.com/open?id=1t02vsbeNskVkIjo75tOOPxZ8VIuxCcrA",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2006",
+        link: "https://drive.google.com/open?id=1nLaWnisfdssFtLPZqpyrcsdziYoCuYYi",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2005",
+        link: "https://drive.google.com/open?id=1BnyYpB1FR3y2jjhOAy-hc9IDYqINLfnw",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2005",
+        link: "https://drive.google.com/open?id=1U61E90vVhGKD-EzOJxWAXpEIBx3Q2ZAc",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2004",
+        link: "https://drive.google.com/open?id=11Nz2XPaHxsK72AdcBa3if3LXB1Ft2lBI",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2004",
+        link: "https://drive.google.com/open?id=1oiqecZovAIpbiEyVnWPImBxftIpdz5Ma",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2003",
+        link: "https://drive.google.com/open?id=1eT_Vq5AIOlX0Ixio1q-3BXNadNps0Nre",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2003",
+        link: "https://drive.google.com/open?id=1r4aODpz3kb9NbFcWzPwALcr6cd24rIB9",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2002",
+        link: "https://drive.google.com/open?id=1M2pSvcKzA7OMFD9b47AZ6I24O44y-Hr2",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2002",
+        link: "https://drive.google.com/open?id=1qL1jIbPNe5QmebBYQEu_sBRFW-gmefzc",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2001",
+        link: "https://drive.google.com/open?id=1kDxyXVQcwhNw1WOw4cNF6EQ86_FNF_M1",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "Two",
+        year: "2001",
+        link: "https://drive.google.com/open?id=1D97BibNm3tNTVzUDHFvUhajlZLqBGZ3T",
+    },
+    {
+        tipe: "ExamPaper",
+        paper: "One",
+        year: "2000",
+        link: "https://drive.google.com/open?id=10ICeC_yOJWala1d66PkWVUOEcpl3OTID",
+    },
+];
 const HLDCGPapers = [
     {
         tipe: "ExamPaper",
